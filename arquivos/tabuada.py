8d1ddffa76dc93059022bed5be823c5126a55170
Nilton Cesar Pereira <cezarpelenilton@gmail.com>
2026-04-24 20:29:07 -0300

    Adicionado exercício de tabuada

---
+++ arquivos/tabuada.py
@@ -0,0 +1,5 @@
+﻿for numero in range(1, 11):   # vai de 1 até 10
+    print(f"\nTabuada do {numero}:")
+    for i in range(1, 11):    # cada tabuada vai de 1 até 10
+        print(f"{numero} x {i} = {numero * i}")
+
