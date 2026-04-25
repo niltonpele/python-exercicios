fc80fa5c84cb8c49ba135c3bfd9df959183ba61b
Nilton Cesar Pereira <cezarpelenilton@gmail.com>
2026-04-24 21:03:41 -0300

    Adicionado exercício de calculadora interativa

---
+++ arquivos/calculadora.py
@@ -0,0 +1,27 @@
+﻿# Programa de calculadora simples
+
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+print("\nEscolha a operação:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+opcao = int(input("Digite a opção: "))
+
+if opcao == 1:
+    print("Resultado:", a + b)
+elif opcao == 2:
+    print("Resultado:", a - b)
+elif opcao == 3:
+    print("Resultado:", a * b)
+elif opcao == 4:
+    if b != 0:
+        print("Resultado:", a / b)
+    else:
+        print("Erro: divisão por zero não é permitida.")
+else:
+    print("Opção inválida.")
+
